2978367bee9b9d89fad0a13351cdc6e8adda7ae7
Navin Bhandari <navinbhandari45@gmail.com>
2021-04-02 15:03:31 +0530

    Page linking abd email check solved

--- foodie/urls.py
+++ foodie/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_navin/', admin.site.urls),
     path('',include('feedback.urls')),
     path('',include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -11,7 +11,17 @@ from django.core.files.storage import FileSystemStorage
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic import View, DetailView
 from users.forms import OutletForm
+import re
 # Create your views here.
+
+
+def is_email_vaild(email):
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex, email)):
+        return True
+
+    else:
+        False
 
 
 class OutletView(View):
@@ -22,58 +32,64 @@ class OutletView(View):
 @login_required
 def registerOutlet(request):
     if request.method == "POST" and request.FILES['outlet_img']:
-        outlet_img = request.FILES['outlet_img']
-        print(outlet_img)
-        fs = FileSystemStorage()
-        filename = fs.save(outlet_img.name, outlet_img)
-        uploaded_file_url = fs.url(filename)
-        user = request.user.username
-        outletname = request.POST.get('OutletName')
-        foodtype = request.POST.get('FoodType')
-        homedelivery = request.POST.get('HomeDelivery')
-        ownerfirstname = request.POST.get('OwnerFirstName')
-        ownerlastname = request.POST.get('OwnerLastName')
-        address = request.POST.get('Address')
-        addresslink = request.POST.get('AddressLink')
         email = request.POST.get('Email')
-        phone = request.POST.get('Phone')
-        outlet = Outlet(outletname=outletname, foodtype=foodtype, homedelivery=homedelivery, ownerfirstname=ownerfirstname, ownerlastname=ownerlastname,
-                        address=address, image=filename, addresslink=addresslink, email=email, phone=phone)
-        outlet.save()
-        messages.success(
-            request, f'Thank You!! Our team will get to you soon!! ')
-        return redirect('home')
+        if (is_email_vaild(email)):
+            outlet_img = request.FILES['outlet_img']
+            fs = FileSystemStorage()
+            filename = fs.save(outlet_img.name, outlet_img)
+            uploaded_file_url = fs.url(filename)
+            user = request.user.username
+            outletname = request.POST.get('OutletName')
+            foodtype = request.POST.get('FoodType')
+            homedelivery = request.POST.get('HomeDelivery')
+            ownerfirstname = request.POST.get('OwnerFirstName')
+            ownerlastname = request.POST.get('OwnerLastName')
+            address = request.POST.get('Address')
+            addresslink = request.POST.get('AddressLink')
+            phone = request.POST.get('Phone')
+
+            outlet = Outlet(outletname=outletname, foodtype=foodtype, homedelivery=homedelivery, ownerfirstname=ownerfirstname, ownerlastname=ownerlastname,
+                            address=address, image=filename, addresslink=addresslink, email=email, phone=phone)
+            outlet.save()
+            messages.success(
+                request, f'Thank You!! Our team will get to you soon!! ')
+            return redirect('home')
+        else:
+            messages.error(
+                request, f'Invalid Email')
+            return render(request, 'login/register_outlet.html')
     return render(request, 'login/register_outlet.html')
 
 
 def signupUser(request):
     if request.method == "POST":
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
         email = request.POST.get('email')
-        password = request.POST.get('password')
-        repassword = request.POST.get('repassword')
-        if password == repassword:
-            try:
-                user = User.objects.get(username=email)
+        if (is_email_vaild(email)):
+            password = request.POST.get('password')
+            repassword = request.POST.get('repassword')
+            if password == repassword:
+                firstname = request.POST.get('firstname')
+                lastname = request.POST.get('lastname')
+                try:
+                    user = User.objects.get(username=email)
+                    messages.error(
+                        request, f'User already exits ')
+                    return render(request, 'login/signup.html')
+                except User.DoesNotExist:
+                    user = User.objects.create_user(
+                        username=email, email=email, password=password)
+                    user.is_active = True
+                    user.first_name = firstname
+                    user.last_name = lastname
+                    user.save()
+                    messages.success(
+                        request, f'Your account created successfully!!')
+                    login(request, user)
+                    return redirect('home')
+            else:
                 messages.error(
-                    request, f'User already exits ')
+                    request, f'Passwords you provided dont match')
                 return render(request, 'login/signup.html')
-            except User.DoesNotExist:
-                user = User.objects.create_user(
-                    username=email, email=email, password=password)
-                user.is_active = True
-                user.first_name = firstname
-                user.last_name = lastname
-                user.save()
-                messages.success(
-                    request, f'Your account created successfully!!')
-                login(request, user)
-                return redirect('home')
-        else:
-            messages.error(
-                request, f'Passwords you provided dont match')
-            return render(request, 'login/signup.html')
     return render(request, 'login/signup.html')
 
 
